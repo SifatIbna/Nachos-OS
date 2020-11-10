37a2bbd04b0609cd5f11940267817193d89d8aea
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2020-11-10 22:01:17 +0600

    message send and receive between listener and speaker successful

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -14,6 +14,14 @@ public class Communicator {
      * Allocate a new communicator.
      */
     public Communicator() {
+
+        myLock = new Lock();
+        speakCon = new Condition2(myLock);
+        listenCon = new Condition2(myLock);
+        returnCon = new Condition2(myLock);
+        myword = 0;
+        hasWord = false;
+
     }
 
     /**
@@ -27,6 +35,18 @@ public class Communicator {
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
+
+        myLock.acquire();
+        while(hasWord)
+        {
+            speakCon.sleep();
+        }
+        myword = word;
+        hasWord = true;
+
+        listenCon.wake();
+        returnCon.sleep();
+        myLock.release();
     }
 
     /**
@@ -36,6 +56,80 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+        myLock.acquire();
+        while(!hasWord)
+        {
+            listenCon.sleep();
+        }
+        speakCon.wake();
+        returnCon.wake();
+        int toReturn = myword;
+        myword = 0;
+        hasWord = false;
+        myLock.release();
+        return toReturn;
+
     }
+
+
+    public static void CommTest() {
+        final Communicator com = new Communicator();
+        final long times[] = new long[4];
+        final int words[] = new int[2];
+        KThread speaker1 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(4);
+                times[0] = Machine.timer().getTime();
+            }
+        });
+        speaker1.setName("S1");
+        KThread speaker2 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(7);
+                times[1] = Machine.timer().getTime();
+            }
+        });
+        speaker2.setName("S2");
+        KThread listener1 = new KThread( new Runnable () {
+            public void run() {
+                times[2] = Machine.timer().getTime();
+                words[0] = com.listen();
+            }
+        });
+
+        listener1.setName("L1");
+        KThread listener2 = new KThread( new Runnable () {
+            public void run() {
+                times[3] = Machine.timer().getTime();
+                words[1] = com.listen();
+            }
+        });
+
+        listener2.setName("L2");
+
+        speaker1.fork(); speaker2.fork(); listener1.fork(); listener2.fork();
+        speaker1.join(); speaker2.join(); listener1.join(); listener2.join();
+
+        Lib.assertTrue(words[0] == 4, "Didn't listen back spoken word.");
+        Lib.assertTrue(words[1] == 7, "Didn't listen back spoken word.");
+        Lib.assertTrue(times[0] > times[2], "speak() returned before listen() called.");
+        Lib.assertTrue(times[1] > times[3], "speak() returned before listen() called.");
+        System.out.println("commTest successful!");
+    }
+
+    public static void selfTest() {
+        System.out.println();
+        System.out.println("Communicator Test>>>>");
+        // place calls to simpler Communicator tests that you implement here
+        CommTest();
+
+    }
+
+
+    private Lock myLock;
+    private Condition2 speakCon;
+    private Condition2 listenCon;
+    private Condition2 returnCon;
+    private int myword;
+    private boolean hasWord;
 }
