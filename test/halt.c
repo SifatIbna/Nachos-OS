f7dbb6295d149f9846093c0b473923cfab9613bf
1abir <1605104@ugrad.cse.buet.ac.bd>
2020-11-11 11:54:06 +0600

    working

--- test/halt.c
+++ test/halt.c
@@ -12,9 +12,14 @@
 
 #include "syscall.h"
 
-int
-main()
+int main()
 {
+    printf("halt started!\n");
+    printf("halt prints a line.\n");
+    printf("halt prints another line.\n");
+    printf("halt tries to halt Nachos\n");
     halt();
+    printf("Nachos not halted!\n");
+    return 0;
     /* not reached */
 }
